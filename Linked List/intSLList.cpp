444d61c6abfc473e44844c948cb097d86da7639c
Saad Hussain <rsaad5095@gmail.com>
2024-08-30 22:03:29 +0500

    added definition of addToTail method

--- Linked List/intSLList.cpp
+++ Linked List/intSLList.cpp
@@ -13,3 +13,11 @@ void IntSLList::addToHead(int _data){
     head = new IntSLLNode(_data, head);
     if (tail == 0) tail = head;
 }
+
+void IntSLList::addToTail(int _data){
+    if(tail != 0){ //if list is not empty
+        tail->next = new IntSLLNode(_data);;
+        tail = tail->next;
+    }
+    else head = tail = new IntSLLNode(_data);
+}
