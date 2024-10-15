298503f29bfbab77fd78a858b7678737d55d8b40
Saad Hussain <rsaad5095@gmail.com>
2024-10-15 23:26:13 +0500

    done nQueens

--- DS Lab Practise/nQueens.cpp
+++ DS Lab Practise/nQueens.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
-bool isSafeToPlace(int **board, int row, int col){
-    
+bool isSafeToPlace(int **board, int row, int col, int n){
+    for(int i=0;i<n;i++){
+        if(board[row][i]) return false;
+    }
+    for(int i=0;i<=row;i++){
+        if(board[i][col]) return false;
+    }
+
+    //diagonals ma check maarlo
+    int i=row;
+    int j=col;
+
+    while(i>=0 && j>=0){
+        if(board[i--][j--]) return false;
+    }
+
+    i=row;
+    j=col;
+
+    while(i>=0 && j<n){
+        if(board[i--][j++]) return false;
+    }
+
+    return true;
 }
 
 bool nQueens(int **board, int n, int row){
@@ -12,16 +35,15 @@ bool nQueens(int **board, int n, int row){
     }
 
     for(int col=0;col<n;col++){
-        //queen ko rakhdo idher
-        board[row][col] = 1;
-        if(isSafeToPlace(board, row, col)){
+        if(isSafeToPlace(board, row, col, n)){
+            board[row][col] = 1;
             //agay wali row ma rakhna chaalo karo
             if(nQueens(board, n, row+1)){
                 //solution mil gya 
                 return true;
             }
         }
-            //hatao is ko agar safe nhi h aur h tu solution nhi mila agar
+            //hatao is ko agar tu solution nhi mila agar
             board[row][col] = 0;
         
 
@@ -31,7 +53,9 @@ bool nQueens(int **board, int n, int row){
 }
 
 int main(){
-    int n=5;
+    int n=22;
+
+    auto before = clock();
 
     //board is unoccupied
     int **board = new int *[n];
@@ -45,4 +69,9 @@ int main(){
             else cout<<".";
         }cout<<endl;
     }
+
+    auto duration= clock() - before;
+    cout << "Duration: " << (float)duration / CLOCKS_PER_SEC << " seconds";
+
+
 }
